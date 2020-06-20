40c3ac22f1e3648f2f2186ede792ef7126751d51
ty0207 <ty0207@sjtu.edu.cn>
2020-06-20 15:30:17 +0800

    sync port information from yurt

--- gconst/const.go
+++ gconst/const.go
@@ -5,9 +5,9 @@ const (
 	YurtImage = "registry.cn-hangzhou.aliyuncs.com/se-devgo/yurt:v2.0"
 
 	// ActionPort is port action server use in container
-	ActionPort = "8000/tcp"
+	ActionPort = "8080/tcp"
 	// SyncPort is port action server use in container
-	SyncPort = "8080/tcp"
+	SyncPort = "8000/tcp"
 	// YurtFilter is for container list
 	YurtFilter = "label=app=thunderyurt"
 
